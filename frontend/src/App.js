5903abdc0c6c1bc607897c9097bd12f1312c4ef8
Peter Cheng <shuoc4@illinois.edu>
2024-03-29 21:41:49 -0500

    feat(redux): Initialize Redux store with user reducer and customized reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Public from './components/Public';
+import Login from './features/auth/Login';
+import DashLayout from './components/DashLayout';
+import Welcome from './features/auth/Welcome';
+import NotesList from './features/notes/NotesList';
+import UsersList from './features/users/UsersList';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>Welcome to my React app.</p>
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
 
-export default App
+        <Route path="dash" element={<DashLayout />}>
+
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+          
+        </Route>
+        {/* End Dash */}
+
+      </Route>
+    </Routes>
+  )
+};
+
+export default App;
